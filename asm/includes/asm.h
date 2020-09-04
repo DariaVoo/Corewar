9002e809d971b72dd71d20aa6d975d8fce91e5d3
Price Kingsblood <pkingsbl@mi-n1.msk.21-school.ru>
2020-09-04 20:00:16 +0300

    g

--- asm/includes/asm.h
+++ asm/includes/asm.h
@@ -23,10 +23,35 @@ typedef struct		s_op
 	int				code_type_arg;
 }					t_op;
 
+//структура для аргумента
+typedef struct s_arg
+{
+	int direct; //аргумент direct - 1 или indirect - 0
+	int size; //размер аргумента (2 или 4) нужен для получения значения
+	int value; //значение, для label рассчитывается при ВТОРОМ проходе
+	int arg_number; //порядковый номер аргумента, возможно, не нужен, но может пригодиться для валидации
+	int instr_num_curr; //порядковый номер текущей инструкции
+	int instr_num_to_go; //порядковый номер инструкции, к которой нужно перескачить (сложить размеры инструкций между двумя значениями)
+	int bc;
+	int reg; //если аргумент является регистром то здесь число, иначе - 0
+}              t_arg;
+
+//инструкции считываемые из файла
+typedef struct s_championstr
+{
+	char *name;
+	int bc;
+	int number; // порядковый номер инструкции, нужен для подсчета лейбла
+	int id;
+	int size; //итоговый размер инструкции, нужен для подсчета лейбла(хз,
+	// может и не надо если размер инструкций с аргументами статичен)
+	t_arg *args;
+}               t_championstr;
+
 typedef struct		s_champion
 {
-	t_header	header;
-	t_championstr *championstr;// пока просто запиши весь код чемпиона сюда
+	t_header        header;
+	t_championstr   *championstr;// пока просто запиши весь код чемпиона сюда
 }					t_champion;
 
 //инструкции, данные в задании (Операции и их аргументы)
@@ -40,46 +65,21 @@ typedef struct s_instr
 
 }               t_instr;
 
-//аргументы операций 
+//аргументы операций
 typedef struct  s_name_args
 {
-    bool    	t_reg;
-    bool    	t_dir;
-    bool    	t_ind;
-    t_name_args	*name_args;
+    int    		t_reg;
+    int    		t_dir;
+    int			t_ind;
+//    t_name_args	*next;
 }               t_name_args;
-
-//инструкции считываемые из файла
-typedef struct s_championstr
-{
-    char *name;
-    int bc;
-    int number; // порядковый номер инструкции, нужен для подсчета лейбла
-    int id;
-    int size; //итоговый размер инструкции, нужен для подсчета лейбла(хз,
-    // может и не надо если размер инструкций с аргументами статичен)
-    t_arg *args;
-}               t_championstr;
-
-//структура для аргумента
-typedef struct s_arg
-{
-    int direct; //аргумент direct - 1 или indirect - 0
-    int size; //размер аргумента (2 или 4) нужен для получения значения 
-    int value; //значение, для label рассчитывается при ВТОРОМ проходе
-    int arg_number; //порядковый номер аргумента, возможно, не нужен, но может пригодиться для валидации
-    int instr_num_curr; //порядковый номер текущей инструкции
-    int instr_num_to_go; //порядковый номер инструкции, к которой нужно перескачить (сложить размеры инструкций между двумя значениями)
-    int bc;
-    int reg; //если аргумент является регистром то здесь число, иначе - 0
-}              t_arg;
 
 //считанные лейблы
 //список
 typedef struct s_label
 {
     char *name;
-    t_champinstr championstr;
+    t_championstr championstr;
     // int label_id; //id лейбла
     // int id; //id инструкции в t_instr заполняется при ВТОРОМ проходе
     // int number; //number в t_champinstr заполняется при ВТОРОМ проходе
@@ -101,5 +101,6 @@ typedef struct s_label
 */
 
 t_champion *parse_champion(char *chmp_file_name);
+int		check_label(char *label);
 
 #endif
