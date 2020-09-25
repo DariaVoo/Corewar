05c5c2fea0acf307b29d5320ca87886693a77f5c
cnails <cnails@student.21-school.ru>
2020-09-25 19:58:05 +0300

    parse_n is ready

--- vm/srcs/parse/parse_n.c
+++ vm/srcs/parse/parse_n.c
@@ -6,88 +6,13 @@
 /*   By: cnails <cnails@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/22 10:44:06 by cnails            #+#    #+#             */
-/*   Updated: 2020/09/22 19:20:20 by cnails           ###   ########.fr       */
+/*   Updated: 2020/09/25 19:54:34 by cnails           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "parse_n.h"
-/*
-*/
 
-static int		ft_numlen(long long nb)
-{
-	if (nb < 0)
-		return (1 + ft_numlen(nb * -1));
-	if (nb >= 10)
-		return (1 + ft_numlen(nb / 10));
-	else
-		return (1);
-}
-
-char			*ft_itoa(long long n)
-{
-	char	*str;
-	int		i;
-
-	i = ft_numlen(n);
-	if (!(str = (char *)malloc(sizeof(char) * (ft_numlen(n) + 1))))
-		return (NULL);
-	str[i--] = '\0';
-	if (n == -2147483648)
-	{
-		str[1] = '2';
-		n = -147483648;
-	}
-	if (n == 0)
-		str[0] = '0';
-	if (n < 0)
-	{
-		n = n * -1;
-		str[0] = '-';
-	}
-	while (n > 0)
-	{
-		str[i--] = n % 10 + '0';
-		n = n / 10;
-	}
-	return (str);
-}
-
-
-int		ft_strlen(char *str) {
-	int i = 0;
-
-	while (str[i]) {
-		i++;
-	}
-	return (i);
-}
-
-/*
-*/
-
-// struct	s_parse_n
-// {
-// 	size_t
-// }		t_parse_n;
-
-int		is_valid_int(int nbr, char *str)
-{
-	char *tmp;
-
-	tmp = ft_itoa(nbr);
-	if (strcmp(tmp, str))
-	{
-		free(tmp);
-		return (0);
-	}
-	return (1);
-}
-
-int		ret_len(int ac, char **av)
+static int		ret_len(int ac, char **av)
 {
 	int		i;
 	int		len;
@@ -105,96 +30,83 @@ int		ret_len(int ac, char **av)
 	return (len);
 }
 
-int		is_not_dupl(t_arg *arg, t_arg *end, int nbr)
+static int		parse_position(int i, t_data *data, t_arg *arg, char **av)
 {
-	t_arg *head;
-
-	head = arg;
-	while (head != end)
+	if (av[i + 1] && is_valid_int(ft_atoi(av[i + 1]), av[i + 1]))
 	{
-		if (head->index == nbr)
-			return (0);
-		head = head->next;
+		arg->index = ft_atoi(av[i + 1]);
+		if (arg->index < data->len && av[i + 2] &&\
+		is_not_dupl(data->arg, arg, arg->index) && arg->index >= 0)
+			arg->name = ft_strdup(av[i + 2]);
+		else
+			ft_exit("invalid options\n");
+		return (2);
 	}
-	return (1);
+	else
+		ft_exit("invalid options\n");
+	return (0);
 }
 
-char	**parse_n(int ac, char **av)
+static void		parse_arg(int ac, char **av, t_data *data)
 {
-	t_arg	*arg;
-	t_arg	*head;
-	char	**res;
-	int		len;
-	int		tmp;
 	int		i;
+	t_arg	*arg;
 
-	tmp = 0;
-	i = 0;
-	len = ret_len(ac, av);
-	printf("len = %d\n", len);
-	arg = (t_arg *)malloc(sizeof(t_arg));
-	head = arg;
-	// if (!(res = (char **)malloc(sizeof(char **) * (len + 1))))
-		// return (NULL);
-	// while (i <= len) {
-		// res[i] = NULL;
-		// i++;
-	// }
+	arg = new_arg();
+	data->arg = arg;
 	i = 1;
 	while (i < ac)
 	{
 		if (!strcmp(av[i], "-n"))
-		{
-			if (av[i + 1] && is_valid_int(atoi(av[i + 1]), av[i + 1]))
-			{
-				arg->index = atoi(av[i + 1]);
-				printf("nbr = %d\n", is_not_dupl(head, arg, arg->index));
-				if (arg->index < len && av[i + 2] && is_not_dupl(head, arg, arg->index))
-					arg->name = strdup(av[i + 2]);
-				else {
-					printf("Error 1\n");
-					exit(1);
-				}
-				i += 2;
-			}
-			else {
-				printf("Error 2\n");
-				exit(1);
-			}
-		}
+			i += parse_position(i, data, arg, av);
 		else
 		{
 			arg->index = -1;
 			arg->name = strdup(av[i]);
 		}
+		i++;
 		if (i < ac)
 		{
-			arg->next = (t_arg *)malloc(sizeof(t_arg));
+			arg->next = new_arg();
 			arg = arg->next;
-			arg->next = NULL;
 		}
-		i++;
 	}
-	while (head->next)
+}
+
+static t_data	*parse_options_n(int ac, char **av)
+{
+	t_data	*data;
+	t_arg	*head;
+
+	data = (t_data *)ft_memalloc(sizeof(t_data));
+	data->len = ret_len(ac, av);
+	parse_arg(ac, av, data);
+	head = data->arg;
+	while (head)
 	{
-		printf("ind = %d\nname = %s\n", head->index, head->name);
+		if (head->index == -1)
+			head->index = first_free_nbr(data->arg, data->len);
 		head = head->next;
 	}
-	return (NULL);
+	return (data);
 }
 
-int main(int ac, char **av)
+char			**parse_n(int ac, char **av)
 {
-	char **str;
-	int i = 0;
+	t_data	*data;
+	t_arg	*tmp;
+	char	**str;
 
-	str = parse_n(ac, av);
-	// while (str[i]) {
-	// 	printf("%s\n", str[i]);
-	// 	free(str[i]);
-	// 	i++;
-	// }
-	return (0);
+	data = parse_options_n(ac, av);
+	if (!(str = (char **)ft_memalloc(sizeof(char *) * (data->len + 1))))
+		ft_exit("no memory\n");
+	while (data->arg)
+	{
+		tmp = data->arg->next;
+		str[data->arg->index] = data->arg->name;
+		free(data->arg);
+		data->arg = tmp;
+	}
+	free(data);
+	return (str);
 }
-
-
--- vm/srcs/parse/parse_n.h
+++ vm/srcs/parse/parse_n.h
@@ -6,7 +6,7 @@
 /*   By: cnails <cnails@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/22 17:45:08 by cnails            #+#    #+#             */
-/*   Updated: 2020/09/22 19:06:43 by cnails           ###   ########.fr       */
+/*   Updated: 2020/09/25 19:57:03 by cnails           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,21 @@
 
 typedef struct	s_arg
 {
-    int				index;
-    char			*name;
-    struct	s_arg	*next;
+	int				index;
+	char			*name;
+	struct s_arg	*next;
 }				t_arg;
+
+typedef struct	s_data
+{
+	t_arg	*arg;
+	int		len;
+}				t_data;
+
+int		is_valid_int(int nbr, char *str);
+int		is_not_dupl(t_arg *arg, t_arg *end, int nbr);
+t_arg	*new_arg(void);
+int		first_free_nbr(t_arg *arg, int len);
+char	**parse_n(int ac, char **av);
 
 #endif
