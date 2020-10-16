9d792e523dde3b047c2b559d6e17b9370446f527
Pa53L <pbeloshein@gmail.com>
2020-10-16 07:46:17 +0300

    фиксим баги

--- asm/includes/error.h
+++ asm/includes/error.h
@@ -1,2 +1,5 @@
 #define LEX_ERR			"Lexical error \n" 		//потом убрать \n
 #define SYN_ERR			"Syntax error \n"
+#define USAGE			"Usage: ./asm [options] <file.s>\n"
+#define HEADER_ERR		"Error: wrong header file\n"
+#define EXEC_MAGIC_ERR	"Error: COREWAR_EXEC_MAGIC > UINT_MAX\n"
--- asm/srcs/check.c
+++ asm/srcs/check.c
@@ -2,53 +2,52 @@
 // Created by pbelo on 12.09.2020.
 //
 #include "../includes/asm.h"
+#include "../includes/error.h"
 #include "../libft/includes/libftprintf.h"
 
 void    ft_check_header_file(void)
 {
-	int i = 0;
-
+	int i;
+	int j;
 	char all_ch[] = {COMMENT_CHAR, LABEL_CHAR, ALT_COMMENT_CHAR, DIRECT_CHAR, SEPARATOR_CHAR};
-	while (i < 5)
+	
+	if (all_ch[0] == '\n' || all_ch[0] == ' ')
+		ft_exit(HEADER_ERR);
+	i = 0;
+	while (i < 4)
 	{
-		int k = i + 1;
-		if (all_ch[i] == '\n')
-			ft_exit("Wrong chars in header\n");
-		while (k < 5)
+		j = i + 1;
+		while (j < 5)
 		{
-			if ( all_ch[k] == '\n' || all_ch[i] == all_ch[k])
-				ft_exit("Wrong chars in header\n");
-			k++;
+			if (all_ch[j] == '\n' || all_ch[i] == all_ch[j] || all_ch[j] == ' ')
+				ft_exit(HEADER_ERR);
+			j++;
 		}
 		i++;
 	}
-//    if (COMMENT_CHAR == '\n' || LABEL_CHAR == '\n' || ALT_COMMENT_CHAR == '\n' || DIRECT_CHAR == '\n' || SEPARATOR_CHAR == '\n')
-//    {
-//        ft_exit("Wrong chars in header\n");
-//
-//        //проверить на ОдИнАкОвОсТь всех этих символов
-//    }
+	if (COREWAR_EXEC_MAGIC > UINT32_MAX)
+		ft_exit(EXEC_MAGIC_ERR);
 }
 
 void	ft_check_args(char **av, int ac)
 {
-    int i = 0;
+	int i = 0;
 
-    if (ac == 1)
-        ft_exit("Usage: bla-bla-bla\n");
-    while (i < ac)
-    {
-        if (i == ac - 1)
-            ft_check_filename(av[i]);
-        else
-            ft_check_flags(av[i]);
-        i++;
-    }
+	if (ac == 1)
+		ft_exit(USAGE);
+	while (i < ac)
+	{
+		if (i == ac - 1)
+			ft_check_filename(av[i]);
+		else
+			ft_check_flags(av[i]);
+		i++;
+	}
 }
 
 void	ft_check_filename(char *name)
 {
-    if (ft_strlen(ft_strstr(name, ".s")) != 2 && ft_strlen(name) < 3)
+    if (ft_strlen(ft_strstr(name, ".s")) != 2)
         ft_exit("Bad file name\n");
     else
         ft_printf("File: %s\n", name);
