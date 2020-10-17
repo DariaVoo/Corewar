48daf6e56a5c506c584925d687b464bc17deaee6
pkingsbl <pkingsbl@student.21-school.ru>
2020-10-17 22:54:29 +0300

    champion

--- asm/Car.s
+++ asm/Car.s
@@ -1,11 +1,10 @@
 #deassembler par msa 1.8, made by joe
 #deassembler par msa 1.8, made by joe
 
+#deassembler par msa 1.8, made by joe
+     .name "Tching tching(Intercepteur), Bouh!Bouh!(bruits d'anti-jeu)"
+.comment "LOL"
 
-    .name				    "Tching tching(Intercepteur), Bouh!Bouh!(bruits d'anti-jeu)"    fdbgdf hidufhv
-
-
-.comment ""
 
 	 fork  %:label00          		# 3,0,281
 	 ld    %-272,r3           		# 7,3,278
