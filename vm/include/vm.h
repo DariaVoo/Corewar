5adb6d38601df3c01419a3a57aba633ed0aae2be
Yoda Shawn <yshawn@ga-p5.msk.21-school.ru>
2020-10-20 00:22:35 +0300

    added error defines

--- vm/include/vm.h
+++ vm/include/vm.h
@@ -13,8 +13,9 @@
 #ifndef VM_H
 # define VM_H
 
-# include "op.h"
+# include "vm_error.h"
 # include "parse_n.h"
+# include "op.h"
 # include "../../lib/include/libft.h"
 # include "../../lib/include/ft_printf.h"
 # include "../../lib/include/get_next_line.h"
@@ -24,6 +25,7 @@
 # include <sys/uio.h>
 # include <fcntl.h>
 
+# define INT_SIZE 4
 ///"live", 1, {T_DIR}, 1, 10, "alive", 0, 0
 // typedef struct s_opt
 // {
@@ -31,54 +33,51 @@
 // 	int		op; // продумать представление операции
 // }				t_opt;
 
-typedef struct s_op			t_op;
 typedef struct s_champion	t_champion;
-typedef struct s_vm			t_vm;
-typedef struct s_carriage	t_carriage;
 
-struct s_carriage
+typedef struct				s_carriage
 {
 	int32_t					regs[REG_NUMBER];
 	int						cycle_to_die;
 	uint8_t					command;
 	int						carry;
 	uint8_t					*position;
-	t_carriage				*next;
-};
+	struct s_carriage		*next;
+}							t_carriage;
 
-struct						s_vm
+typedef struct				s_vm
 {
 	uint8_t					arena[MEM_SIZE];
 	uint32_t				dump_cycle_to_die;
-	
+	int						count_champs;
 	t_champion				*champs;
-	size_t					col_champs;
-	t_carriage				*cursor;
-};
+	struct s_carriage		*cursor;
+}							t_vm;
 
 
-struct						s_op
+typedef struct				s_op
 {
-	char	*name_oper;
-	int		col_arg;
-	int		*type_arg;
-	int		opcode;
-	int		cycle_to_die;
-	char	*comment;
-	int		change_carry;
-	int		code_type_arg;
-	int		(*func)(uint32_t arg1, uint32_t arg2, uint32_t arg3, t_vm *vm);
-};
+	char					*name_oper;
+	int						col_arg;
+	int						*type_arg;
+	int						opcode;
+	int						cycle_to_die;
+	char					*comment;
+	int						change_carry;
+	int						code_type_arg;
+	int	(*func)(uint32_t arg1, uint32_t arg2, uint32_t arg3, t_vm *vm);
+}							t_op;
 
 struct						s_champion
 {
-	t_header	header;
-	int			id;
-	uint8_t		*code; // пока просто запиши весь код чемпиона сюда
-	t_champion	*next;
+	t_header				header;
+	int						id;
+	uint8_t					*code; // пока просто запиши весь код чемпиона сюда
+	struct s_champion		*next;
 };
 
-void						ft_exit(char *str);
+/* VIRTUAL MACHINE MAIN */
+t_vm						*create_vm(int count_champs);
 t_champion					*init_champ(int id);
 t_vm						*init_vm(size_t col_champs);
 void						init_arena(t_vm *vm);
@@ -86,5 +85,8 @@ t_carriage					*init_carrige(int id);
 t_champion					*parse_champion(char *chmp_file_name, int id);
 t_champion					*valid_champions(char **chmp_file_name, size_t col_champs);
 t_carriage					*valid_cursor(t_champion *players);
+
+/* EXTRA FUNC */
+void						exit_error(const char *str);
 
 #endif
---
+++ vm/include/vm_error.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vm.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qjosmyn <qjosmyn@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/31 14:52:46 by qjosmyn           #+#    #+#             */
+/*   Updated: 2020/10/10 16:30:55 by qjosmyn          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VM_ERROR_H
+# define VM_ERROR_H
+
+# define E_MALLOC "in malloc"
+# define E_ERR "Error: "
+# define E_PAR_MAGIC_HEADER "Error magic header"
+# define E_PAR_NAME "Read name"
+# define E_PAR_CHAMP_SIZE "Read champ size"
+# define E_PAR_COMMENT "Read comment"
+
+#endif
