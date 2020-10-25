8b4aad5351487b1f18fa198c0b67a4da09a98d79
vsezanatodazheeto <kirill.glinsky@gmail.com>
2020-10-25 20:00:45 +0300

    added file for extra functions (print, debug, etc)

---
+++ vm/src/extra/extra.c
@@ -0,0 +1,42 @@
+#include "vm.h"
+
+void		exit_error(const char *str)
+{
+	// perror(str);
+	ft_printf("%s%s\n", E_ERR, str);
+	exit(0);
+	// exit(EXIT_FAILURE);
+}
+
+void		champ_print(t_champion *ptr)
+{
+	unsigned int i;
+
+	i = 0;
+	ft_printf("Magic header: %x\nNAME: %s\nProg size: %x\nComment: %s\nCode:\n", ptr->header.magic, ptr->header.prog_name, ptr->header.prog_size, ptr->header.comment);
+	while (i < ptr->header.prog_size)
+	{
+		ft_printf("%.2x", (0xff & ptr->code[i]));
+		if (i % 2 != 0)
+			ft_putstr(" ");
+		if ((i + 1) % 16 == 0 && i != 0)
+			ft_putstr("\n");
+		i++;
+	}
+}
+
+void		arena_print(uint8_t *arena)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < MEM_SIZE)
+	{
+		ft_printf("%.2x", (0xff & arena[i]));
+		if (i % 2 != 0)
+			ft_putstr(" ");
+		if ((i + 1) % 64 == 0 && i != 0)
+			ft_putstr("\n");
+		i++;
+	}
+}
