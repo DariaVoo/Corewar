079b3c732817aacfe011643ce9d1489713d026ea
Yoda Shawn <yshawn@ox-p3.msk.21-school.ru>
2020-10-30 22:45:25 +0300

    debug functions

---
+++ vm/src/debug/debug.c
@@ -0,0 +1,54 @@
+#include "vm.h"
+
+void		champ_print(t_champion *ptr)
+{
+	unsigned int i;
+
+	i = 0;
+	ft_printf("Magic header: %x\nNAME: %s\nProg size: %x\nComment: %s\nCode:\n", ptr->header.magic, ptr->header.prog_name, ptr->header.prog_size, ptr->header.comment);
+	while (i < ptr->header.prog_size)
+	{
+		ft_printf("%.2x", (0xff & ptr->code[i]));
+		if (i % 2 != 0)
+			ft_putstr(" ");
+		if ((i + 1) % 16 == 0 && i != 0)
+			ft_putstr("\n");
+		i++;
+	}
+}
+
+void		arena_print(uint8_t *arena)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < MEM_SIZE)
+	{
+		if (arena[i])
+			ft_printf("{red}%.2x{eoc}", (0xff & arena[i]));
+		else
+			ft_printf("%.2x", (0xff & arena[i]));
+		if (i % 2 != 0)
+			ft_putstr(" ");
+		if ((i + 1) % 64 == 0 && i != 0)
+			ft_putstr("\n");
+		i++;
+	}
+}
+
+void			debug_print_carriage(t_vm *vm)
+{
+	t_carriage	*copy;
+
+	copy = vm->carriage;
+	while (copy)
+	{
+		for (int i = 0; i < REG_NUMBER; i++)
+			ft_printf("%d ", copy->regs[i]);
+		ft_printf("\n");
+		ft_printf("current_pos: %d\n", copy->program_counter);
+		ft_printf("%.2x\n", copy->opcode);
+		ft_printf("-------------------------------------\n");
+		copy = copy->next;
+	}
+}
