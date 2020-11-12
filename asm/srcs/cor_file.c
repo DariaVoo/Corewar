b97b0c53c4034038e6400c55a92135553f99b31f
Redbeard Retta <rretta@ox-p5.msk.21-school.ru>
2020-11-13 00:16:31 +0300

    Open file managment

--- asm/srcs/cor_file.c
+++ asm/srcs/cor_file.c
@@ -1,17 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cor_file.c                                         :+:      :+:    :+:   */
+/*   s_to_cor_file.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rretta <rretta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 23:00:36 by rretta            #+#    #+#             */
-/*   Updated: 2020/11/04 23:08:37 by rretta           ###   ########.fr       */
+/*   Updated: 2020/11/12 22:04:59 by rretta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 #include "../libft/includes/libftprintf.h"
+
+void	ft_check_filename(char *name)
+{
+	if (ft_strlen(ft_strstr(name, ".s")) != 2)
+		ft_exit("Bad file name\n");
+	else
+		ft_printf("File: %s\n", name);
+}
 
 char	*ft_cor_extension(char *file)
 {
