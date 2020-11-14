28060ed38840330fccb556d51844492bb9d8e53b
Leta <pkingsbl@student.21-school.ru>
2020-11-14 17:34:52 +0300

    cleaning shit

--- asm/srcs/main.c
+++ asm/srcs/main.c
@@ -40,35 +40,7 @@ int		main(int ac, char **av)
 
     ft_close_fd(all_data.read_fd, all_data.write_fd);
 
-
-
-
-    // char	**split;
-    // int i;
-    // t_header 	header;
-    // t_body		body;
-
-    // i = 0;
-    // split = NULL;
-
-    // ft_check_header_file();
-    // ft_check_args(av, ac);
-    // split = ft_file_read(split, av[ac - 1]);
-    // split = file_comment_trim сделать сплит по COMMENT_CHAR и ALT_COMMENT_CHAR
-
-//    while (*split != NULL)
-//    {
-//        ft_printf("%s\n", *split);
-//        split++;
-//    }
-
-	// i = ft_parse_header(split, &header);
-	// ft_printf("line_number == %d | header.prog_size == %d | header.prog_name == %s\n", i, header.prog_size, header.prog_name);
-
-
 	// TODO Заменить заглушку
-	// ft_parse_body(split, i, 1000);
-	// ft_create_body
 //    ft_printf("FINISH\n");
     free_data(&all_data);
 	return (0);
