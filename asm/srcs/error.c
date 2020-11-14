eb2126f513a52315bfa2812069d4a4e9819f2365
Question Josmyn <qjosmyn@mi-r1.msk.21-school.ru>
2020-11-14 21:18:31 +0300

    norm

--- asm/srcs/error.c
+++ asm/srcs/error.c
@@ -1,11 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qjosmyn <qjosmyn@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/14 21:16:37 by qjosmyn           #+#    #+#             */
+/*   Updated: 2020/11/14 21:18:05 by qjosmyn          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "asm.h"
 #include "../libft/includes/libftprintf.h"
 
-/*
- * переделать на fd 2 (stderr)
- */
-
-void    ft_error(char *err, char *line, int num_str, int ind_str)
+void	ft_error(char *err, char *line, int num_str, int ind_str)
 {
 	int		i;
 
@@ -15,11 +23,13 @@ void    ft_error(char *err, char *line, int num_str, int ind_str)
 	ft_putnbr_fd(num_str, 2);
 	ft_putstr_fd(":", 2);
 	ft_putnbr_fd(ind_str, 2);
-	ft_putstr_fd("]", 2);// добавить принтф с фд
-	if (line) {
+	ft_putstr_fd("]", 2);
+	if (line)
+	{
 		write(2, " INSTRUCTION \"", 14);
-		while (line[i] != ' ' && line[i] != '\t' && line[i] != COMMENT_CHAR &&
-			   line[i] != ALT_COMMENT_CHAR && line[i] != '\0') {
+		while (line[i] != ' ' && line[i] != '\t' && line[i] != COMMENT_CHAR && \
+							line[i] != ALT_COMMENT_CHAR && line[i] != '\0')
+		{
 			ft_putchar_fd(line[i], 2);
 			i++;
 		}
@@ -30,17 +40,16 @@ void    ft_error(char *err, char *line, int num_str, int ind_str)
 	exit(1);
 }
 
-/*
- * ошибка размера имени(0) или комментария(1)
- */
 void	ft_error_length(int f)
 {
-	if (f == 0) {
+	if (f == 0)
+	{
 		ft_putstr_fd("Champion name too long (Max length ", 2);
 		ft_putnbr_fd(PROG_NAME_LENGTH, 2);
 		ft_putendl_fd(")", 2);
 	}
-	else if (f == 1) {
+	else if (f == 1)
+	{
 		ft_putstr_fd("Champion comment too long (Max length ", 2);
 		ft_putnbr_fd(COMMENT_LENGTH, 2);
 		ft_putendl_fd(")", 2);
@@ -62,7 +71,7 @@ void	ft_error_parse_body(char *err, char *line, int line_count, int ind_str)
 	exit(1);
 }
 
-void	free_close_fd_put_error(char *err, char *line, t_data *data, int ind_str)
+void	free_fd_put_error(char *err, char *line, t_data *data, int ind_str)
 {
 	int line_count;
 
