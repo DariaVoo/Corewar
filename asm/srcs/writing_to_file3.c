1691b004516408f93e381a05311d92df0b4d4036
Leta <pkingsbl@student.21-school.ru>
2020-11-14 22:26:22 +0300

    bc norm

--- asm/srcs/writing_to_file3.c
+++ asm/srcs/writing_to_file3.c
@@ -42,3 +42,47 @@ int		writing_to_file(t_data *data, int fd)
 	write_instrs_to_fd(data, fd);
 	return (0);
 }
+
+void	write_args_to_fd(t_data *data, int ind_instr, int code_op, int fd)
+{
+	int		i;
+	int		size;
+	t_arg	*args;
+	t_instr *instrs;
+
+	i = 0;
+	size = 0;
+	instrs = &data->instrs[ind_instr];
+	args = data->instrs[ind_instr].args;
+	while (i < g_op_tab[code_op - 1].col_args)
+	{
+		if (args[i].type == T_REG)
+			ft_putchar_fd(args[i].value, fd);
+		else
+		{
+			size = size_to_label(data, &args[i], instrs->sum_size \
+							- instrs->size, g_op_tab[code_op - 1].tdir_size);
+			write_code_dir(size, args[i].type, fd, \
+											g_op_tab[code_op - 1].tdir_size);
+		}
+		i++;
+	}
+}
+
+void	write_instrs_to_fd(t_data *data, int fd)
+{
+	int		i;
+	int		code_op;
+
+	i = 0;
+	code_op = 0;
+	while (i < data->instr_num)
+	{
+		code_op = code_operation(data->instrs[i].name);
+		ft_putchar_fd(code_op, fd);
+		if (g_op_tab[code_op - 1].bit_type == 1)
+			ft_putchar_fd(code_args(data->instrs[i].args), fd);
+		write_args_to_fd(data, i, code_op, fd);
+		i++;
+	}
+}
